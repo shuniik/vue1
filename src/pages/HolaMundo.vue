61d9e42b6ced15848209b1e1a7e3cebd4321907f
Juan hola work <shunikv@gmail.com>
2024-04-17 15:18:37 -0600

    propiedades reactivas, objeto

---
+++ src/pages/HolaMundo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Hola Mundo a todos!!!!</h2>
+    <h5>{{ nombre }}</h5>
+
+    <q-input v-model="nombre"></q-input>
+    <q-btn @click="modificar">Modificar</q-btn>
+    <br />
+    <h5>Pais: {{ direccion.pais }}</h5>
+    <h5>Departamento: {{ direccion.departamento }}</h5>
+    <h5>Zona: {{ direccion.zona }}</h5>
+    <h5>{{ direccion }}</h5>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const nombre = ref("Libreta");
+
+const direccion = ref({
+  pais: "Guatemala",
+  departamento: "Quetzaltenango",
+  zona: "9",
+});
+
+const modificar = () => {
+  nombre.value = "cambiando los datos";
+  direccion.value.municipio = "Ciudad";
+};
+</script>
+<style lang=""></style>
