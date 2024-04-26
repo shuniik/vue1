8db8fb4dd0a7cad97733f048b9daf4a065cac073
Juan hola work <shunikv@gmail.com>
2024-04-26 14:24:32 -0600

    slot y props

---
+++ src/components/TareaDetalle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <slot></slot>
+
+    <strong>
+      <div>
+        {{ propiedades.nombre }} - {{ propiedades.completado }}
+        <q-btn
+          @click="$emit('eliminar', propiedades.nombre)"
+          icon="delete"
+          size="sm"
+          round
+        ></q-btn>
+      </div>
+    </strong>
+  </div>
+</template>
+
+<script setup>
+// const propiedades = defineProps(["nombre", "completado"]);
+const propiedades = defineProps({
+  nombre: String,
+  completado: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+});
+</script>
+
+<style></style>
---
+++ src/pages/componenteVue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-ma-lg"></div>
+  <div v-for="(tarea, key) in tareas" :key="key" class="q-pa-lg">
+    <tareaDetalle
+      @eliminar="eliminarTarea"
+      :nombre="tarea.nombre"
+      :completado="tarea.completado"
+    >
+      {{ tarea.nombre }} slot {{ tarea.completado }}
+    </tareaDetalle>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import tareaDetalle from "../components/TareaDetalle.vue";
+
+const tareas = ref([
+  { nombre: "Aprender vue", completado: false },
+  { nombre: "Aprender Firebase", completado: false },
+  { nombre: "Aprender css", completado: false },
+  { nombre: "Aprender html", completado: false },
+  { nombre: "Aprender JavaScript", completado: false },
+]);
+
+const eliminarTarea = (nombre) => {
+  console.log("eliminarTarea");
+  console.log(nombre);
+  tareas.value = tareas.value.filter((tarea) => tarea.nombre !== nombre);
+};
+</script>
+
+<style></style>
